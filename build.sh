897295b7be0520e6e499cc2b0b954b56a90231c3
Ravindra <ravindrarsawant@gmail.com>
2026-03-03 21:16:42 -0800

    Fix Playwright browser install cache path on Render

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# Store Playwright browsers in a location Render can cache them
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright
+
 pip install -r requirements.txt
 playwright install chromium
 playwright install-deps chromium
